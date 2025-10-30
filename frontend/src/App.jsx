e204decdb1d67cec4cc6d3f64ddfaee8905147a4
its-me-abhiram <abhiram143sag@gmail.com>
2025-10-30 14:53:12 +0530

    deploying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <h1>Task Tracker</h1>
-      <TaskForm onAdd={() => window.location.reload()} />
+      <TaskForm  />
       <TaskList />
       <InsightsPanel />
     </div>
